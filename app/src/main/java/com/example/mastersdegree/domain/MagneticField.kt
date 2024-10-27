30ca401d0a2fa1f8c455704610214144beadd224
Rafael Mirhaydarov <raffaellmir@gmail.com>
2024-10-28 02:07:31 +0500

    MagneticSensorManager

--- app/src/main/java/com/example/mastersdegree/domain/MagneticField.kt
+++ app/src/main/java/com/example/mastersdegree/domain/MagneticField.kt
@@ -1,9 +1,5 @@
 package com.example.mastersdegree.domain
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
-
 data class MagneticField(
     val x: Float = 0.0F,
     val y: Float = 0.0F,
